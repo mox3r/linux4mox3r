7a3cbb4e57976d222cd9c5a27371326bd400f2fd
mox3r <mox3r.box@gmail.com>
2024-09-28 15:45:03 +0300

    ver 1.105

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -4,6 +4,7 @@
 # Set defaults
 # ----------------------------------------------------- 
 
+force_generate=0
 generated_versions="$HOME/.cache/l4m-wallpaper-generated"
 cache_file="$HOME/.cache/current_wallpaper"
 blurred_wallpaper="$HOME/.cache/blurred_wallpaper.png"
@@ -26,7 +27,11 @@ cd Pictures/wallpaper
 cp $(ls | shuf -n 1) $generated_versions/default.jpg
 cd ~
 
-
+# ----------------------------------------------------- 
+# Get wallpaper filename
+# ----------------------------------------------------- 
+wallpaper_filename=$(basename $wallpaper)
+echo ":: Wallpaper Filename: $wallpaper_filename"
 
 # ----------------------------------------------------- 
 # Execute pywal
@@ -40,8 +45,8 @@ source "$HOME/.cache/wal/colors.sh"
 # Write hyprpaper.conf
 # -----------------------------------------------------
 
-echo ":: Setting wallpaper"
-killall -e hyprpaper & 
+echo ":: Setting wallpaper with $used_wallpaper"
+killall hyprpaper 
 hyprpaper & > /dev/null 2>&1
 
 
@@ -49,23 +54,23 @@ hyprpaper & > /dev/null 2>&1
 # Created blurred wallpaper
 # -----------------------------------------------------
 
-echo ":: Generate new cached wallpaper blur-$blur-default with blur $blur"
+echo ":: Generate new cached wallpaper blur-$blur-$wallpaper_filename with blur $blur"
 magick $wallpaper -resize 75% $blurred_wallpaper
 echo ":: Resized to 75%"
 if [ ! "$blur" == "0x0" ] ;then
     magick $blurred_wallpaper -blur $blur $blurred_wallpaper
-    cp $blurred_wallpaper $generated_versions/blur-$blur-default.png
+    cp $blurred_wallpaper $generated_versions/blur-$blur-$wallpaper_filename.png
     echo ":: Blurred"
 fi
-cp $generated_versions/blur-$blur-default.png $blurred_wallpaper
+cp $generated_versions/blur-$blur-$wallpaper_filename.png $blurred_wallpaper
 
 
 # ----------------------------------------------------- 
 # Created square wallpaper
 # -----------------------------------------------------
 
-echo ":: Generate new cached wallpaper square-default"
+echo ":: Generate new cached wallpaper square-$wallpaper_filename"
 magick $wallpaper -gravity Center -extent 1:1 $square_wallpaper
-cp $square_wallpaper $generated_versions/square-default.png
+cp $square_wallpaper $generated_versions/square-$wallpaper_filename.png
 notify-send -u low -t 1000 "Wallpaper change..." "Done!"
 
